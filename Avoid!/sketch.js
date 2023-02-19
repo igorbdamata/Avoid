07ec862757ab1f82afdde4299dedf861722223a8
igorbdamata <igorbdamata@gmail.com>
2023-02-19 16:52:59 -0300

    Display text function

--- Avoid!/sketch.js
+++ Avoid!/sketch.js
@@ -3,10 +3,16 @@ let height = 480;
 
 let player;
 let enemies = [];
-let maxEnemiesLength =5;
+let maxEnemiesLength = 5;
 let currentScore = 0;
 let scoreOnLastSpawn = 0;
 let startGameTime;
+
+function displayText(color, size, textToDiplay, position) {
+  fill(color);
+  textSize(size);
+  text(textToDiplay, position.x, position.y);
+}
 
 function CreateEnemy() {
   let posX = random(0, width);
@@ -20,8 +26,8 @@ function initGame() {
   let playerSpeed = 3;
   let playerPosition = createVector(width / 2, height / 2);
   player = new Player(playerPosition, playerDiameter, playerSpeed, createVector(width, height), '#E6E9FE');
-  scoreOnLastSpawn=0;
-  currentScore=0;
+  scoreOnLastSpawn = 0;
+  currentScore = 0;
   enemies = [];
   CreateEnemy();
 }
@@ -32,10 +38,8 @@ function setup() {
 }
 
 function AnyEnemyHaveHitPlayer() {
-  for(let i =0; i< enemies.length; i++)
-  {
-    if(enemies[i].haveHitPlayer)
-    {
+  for (let i = 0; i < enemies.length; i++) {
+    if (enemies[i].haveHitPlayer) {
       return true;
     }
   }
@@ -46,23 +50,20 @@ function draw() {
   if (!AnyEnemyHaveHitPlayer()) {
     background('#1E1F21');
     enemies.forEach(element => {
-      element.update(); 
+      element.update();
       element.show();
     });
 
     player.update();
     player.show();
-    currentScore = round((performance.now()-startGameTime)/100);
-    if(enemies.length<maxEnemiesLength && currentScore-scoreOnLastSpawn>=100)
-    {
-      scoreOnLastSpawn=currentScore;
+    currentScore = round((performance.now() - startGameTime) / 100);
+    if (enemies.length < maxEnemiesLength && currentScore - scoreOnLastSpawn >= 100) {
+      scoreOnLastSpawn = currentScore;
       CreateEnemy();
     }
-    fill(color('#E6E9FE'));
+
     let size = 30;
-    let textToDisplay = 'Score: ' + currentScore;
-    textSize(size);
-    text(textToDisplay, 20, size+20);
+    displayText('#E6E9FE', size, 'Score: ' + currentScore, createVector(20, size+20))
   }
   else {
     OnGameOver();
@@ -70,17 +71,17 @@ function draw() {
 }
 
 function OnGameOver() {
-  fill(color('#E6E9FE'));
   background('#992F0F');
+
   let size = 64;
   let textToDisplay = 'Game Over!';
-  textSize(size);
-  text(textToDisplay, (width - textToDisplay.length / 2 * size) / 2, height / 2);
+  let textPosition = createVector((width - textToDisplay.length / 2 * size) / 2, height / 2);
+  displayText('#E6E9FE', size, textToDisplay, textPosition)
 
   size = 32;
   textToDisplay = 'Press any key!';
-  textSize(size);
-  text(textToDisplay, (width - textToDisplay.length / 2 * size) / 2, height / 2 + 100);
+  textPosition = createVector( (width - textToDisplay.length / 2 * size) / 2, height / 2 + 100);
+  displayText('#E6E9FE', size, textToDisplay, textPosition)
 }
 function RestartGame() {
   initGame();
